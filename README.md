9c90d2de3a38c642ed29ef59a90520cf88b22ae7
Kevin McAleer <kevinmcaleer@gmail.com>
2023-12-31 16:29:27 +0000

    Ready for Livestream

---
+++ README.md
@@ -0,0 +1,5 @@
+#
+This Python code will take the video stream from an ESP32Cam and process it for faces on another, connected computer.
+
+Libraries needed on the host computer:
+`pip install mediapipe, cvzone, opencv-python`
--- stream.py
+++ stream.py
@@ -1,7 +1,11 @@
 import cv2
+import cvzone
+from cvzone import FaceDetectionModule
+face_detector = FaceDetectionModule.FaceDetector()
+
 
 # Replace with your RTSP stream URL
-rtsp_url = 'rtsp://your_esp32_cam_stream'
+rtsp_url = 'http://192.168.4.1:81/stream'
 
 # Capture the video stream
 cap = cv2.VideoCapture(rtsp_url)
@@ -13,10 +17,9 @@ while True:
         break
 
     # Process the frame with OpenCV here
-    # ...
-
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+    frame, list_faces = face_detector.findFaces(frame)
+    cv2.imshow("Face Detection", frame)
+    
     if cv2.waitKey(1) == ord('q'):
         break
 
